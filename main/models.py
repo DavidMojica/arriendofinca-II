ad4bfafc723d4347378d09f8440e7bad4077685d
David Mojica <davidmojicav@gmail.com>
2024-02-12 15:04:01 -0500

    10/13 models

--- main/models.py
+++ main/models.py
@@ -16,6 +16,51 @@ class TipoDocumento(models.Model):
     def __str__(self):
         return self.desc
 
+class TipoInmueble(models.Model):
+    id = models.AutoField(primary_key=True)
+    desc = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.desc
+    
+class ArriendoVenta(models.Model):
+    id = models.AutoField(primary_key=True)
+    desc = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.desc
+    
+class Certificado(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    desc = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.desc
+
+class Pais(models.Model):
+    id = models.AutoField(primary_key=True)
+    desc = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.desc
+    
+class Departamento(models.Model):
+    id = models.AutoField(primary_key=True)
+    desc = models.CharField(max_length=10)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.desc
+
+class Municipio(models.Model):
+    id = models.AutoField(primary_key=True)
+    desc = models.CharField(max_length=10)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.desc
+    
+    
 # Login Model #
 class Usuario(AbstractUser):
     groups = models.ManyToManyField(Group, related_name="customuser_set")
@@ -32,5 +77,18 @@ class Usuario(AbstractUser):
             self.set_unusable_password()
         super(Usuario, self).save(*args, **kwargs)
         
-        
-
+# Inmuebles #
+class Inmueble(models.Model):
+    id = models.AutoField(primary_key=True)
+    arriendo_venta = models.ForeignKey(ArriendoVenta, on_delete = models.CASCADE)
+    municipio_ubicacion = models.ForeignKey(Municipio, on_delete = models.CASCADE)
+    tipo_inmueble = models.ForeignKey(TipoInmueble, on_delete = models.CASCADE)
+    precio = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=80)
+    descripcion = models.CharField(max_length=300)
+    duenio = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    area = models.CharField(max_length=7)
+    area_construida = models.CharField(max_length=7)
+    habitaciones = models.CharField(max_length=3)
+    banios = models.CharField(max_length=3)
+    certificado = models.ForeignKey(Certificado, on_delete=models.CASCADE)
