1f8b7f03036714f6f544439b6dc5ccffca38766c
David Mojica <davidmojicav@gmail.com>
2024-02-12 12:58:53 -0500

    creación usuario

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission, BaseUserManager
 
-# Create your models here.
+class TipoUsuario(models.Model):
+    id = models.IntegerField(primary_key=True)
+    desc = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.desc
+
+
+# Login Model #
+class usuario(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name="customuser_set")
+    user_permissions = models.ManyToManyField(Permission, related_name="customuser_set")
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete = models.CASCADE, default = 1)
+    
+    def save(self, *args, **kwargs):
+        if not self.password:
+            self.set_unusable_password()
+        super(usuario, self).save(*args, **kwargs)
+
