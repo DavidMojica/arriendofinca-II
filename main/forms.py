61caa0a987452ae5c9b92e954769c930568ccb9f
David Mojíca <102682432+DavidMojica@users.noreply.github.com>
2024-04-10 11:26:09 -0500

    login - Seg Inf updates

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
-from .models import TipoInmueble, ArriendoVenta, Municipio
+from .models import TipoInmueble, ArriendoVenta, Municipio, Usuario
 from django import forms
 from django.db import models
 
@@ -6,21 +6,24 @@ class BusquedaInmuebleForm(forms.Form):
     tipo_inmueble = forms.ModelChoiceField(
         label="Busco un(a)",
         widget=forms.Select(attrs={'class':'form-select'}),
-        queryset=TipoInmueble.objects.all(),
+        # queryset=TipoInmueble.objects.all(),
+        queryset=None,
         empty_label="Seleccionar",
         required=True
     )
     arriendo_venta = forms.ModelChoiceField(
         label="Para",
         widget=forms.Select(attrs={'class':'form-select'}),
-        queryset=ArriendoVenta.objects.all(),
+        # queryset=ArriendoVenta.objects.all(),
+        queryset=None,
         empty_label="Seleccionar",
         required=True
     )
     municipio = forms.ModelChoiceField(
         label="Ubicación",
         widget=forms.Select(attrs={'class':'form-select'}),
-        queryset=Municipio.objects.all(),
+        # queryset=Municipio.objects.all(),
+        queryset=None,
         empty_label="Seleccionar",
         required=True
     )
@@ -29,3 +32,48 @@ class BusquedaInmuebleForm(forms.Form):
         widget=forms.CheckboxInput(attrs={'class': ''}),
         required=False
     )
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label="Usuario",
+        required=True,
+        widget=forms.TextInput(attrs={'id':'username'})
+    )
+
+    password = forms.CharField(
+        label="Contraseña",
+        required=True,
+        widget= forms.PasswordInput(attrs={'id':'password'})
+    )
+
+    class Meta:
+        fields = ('username', 'password')
+
+class RegisterForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label="Nombre",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el nombre del empleado/administrador', 'id': 'nombre'})
+    )
+    
+    last_name = forms.CharField(
+        label="Apellidos",
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese los apellidos', 'id':'apellidos'})
+    )
+
+    username=forms.CharField(
+        label='Nombre de usuario',
+        widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Documento del usuario', 'id':'documento'})
+    )
+    
+    password = forms.CharField(
+        label="Contraseña",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Nueva contraseña', 'id': 'password'})
+    )
+
+    email = forms.EmailField(
+        label="Correo electrónico",
+        widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder': 'Correo electrónico', 'id': 'email'})
+    )
+
+    class Meta:
+        fields = ('first_name', 'last_name', 'username', 'password')
--- main/views.py
+++ main/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import BusquedaInmuebleForm
+from .forms import BusquedaInmuebleForm, LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout
+
+#Variables
+USERLENGTHMIN = 4
+PASSLENGTHMIN = 8
 
 #HTTDOCS
 HTMLHOME = 'home.html'
 HTMLLOGIN = 'login.html'
+
+#MENSAJES
+ERROR_1 = "El nombre de usuario ya existe."
+ERROR_3 = "Error desconocido."
+ERROR_2 = "Formulario inválido."
+ERROR_4 = "Usuario o contraseña incorrecta."
+ERROR_6 = "Usuario o documento demasiado corto(s)."
+
+#Funcitions
+#Decorador que valida que el usuario no esté logueado para hacer algo.
+def unloginRequired(view_func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('orders')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper
 
 # Create your views here.
 def home(request):
@@ -16,7 +38,59 @@ def home(request):
 
 def login(request):
     data = {
-        
+        'newLoginForm': LoginForm(),
+        'newRegisterForm': RegisterForm()
     }
+
+    if request.method == 'POST':
+        if 'login' in request.POST:
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+
+                if len(username) < USERLENGTHMIN or len(password) < PASSLENGTHMIN:
+                    recycledForm = LoginForm(initial={'username': username})
+                    return render(request, HTMLLOGIN, {'form': recycledForm,'error': ERROR_6})        
+                
+                logedUser = authenticate(request, username=username, password=password)
+
+                if logedUser is None:
+                    recycledForm = LoginForm(initial={'username': username})
+                    return render(request, HTMLLOGIN, {'form': recycledForm,'error':ERROR_4})
     
-    return render(request, HTMLLOGIN, {**data})
+                login(request, logedUser)
+            else:
+                data['error'] = ERROR_2
+                return render(request, HTMLLOGIN, {**data})
+        elif 'register' in request.POST:    
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                data['recycledRegisterForm'] = form
+
+
+                if form.has_error('username', code="unique"):
+                    data['error'] = ERROR_1
+                    return render(request, HTMLLOGIN, {**data})
+
+                try:
+                    event = None
+                    username = form.cleaned_data['username']
+                    password = form.cleaned_data['password']
+                    tipo_usuario = 0
+
+                    if len(username) < USERLENGTHMIN or len(password) < PASSLENGTHMIN:
+                        data['error'] = ERROR_6
+                        return render(request, HTMLLOGIN, {**data})
+                    
+                    user = form.save(commit=False)
+                    user.username = username
+                    user.set_password(password)
+                    user.email = form.cleaned_data['email']
+
+                except Exception as e:
+                    data['error'] = ERROR_3
+                    return render(request, HTMLLOGIN, {**data})
+    #Get
+    else:
+        return render(request, HTMLLOGIN, {**data})
