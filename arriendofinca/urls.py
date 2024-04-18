515e3c5ccdaa81bc9f5469ceb54d5b0d9bca03d6
David Mojica <davidmojicav@gmail.com>
2024-04-18 17:17:06 -0500

    Userarea vista de inmuebles front

--- arriendofinca/urls.py
+++ arriendofinca/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('crear_inmueble/', views.CrearInmueble, name="crear_inmueble"),
     #APIS#
     path('municipios_por_departamento/', views.municipios_por_departamento, name='municipios_por_departamento')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ main/static/js/user_area.js
@@ -0,0 +1,6 @@
+const default_pics = document.querySelectorAll('.default-inmueble-pic');
+const tetrade_palette = ['#ffa500', '#3fc1b4', '#2e1377', '#12192C', '#4089ce'];
+
+default_pics.forEach((pic)=>{
+    pic.style.color = tetrade_palette[Math.floor(Math.random() * tetrade_palette.length)]
+});
