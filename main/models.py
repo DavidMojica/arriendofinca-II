eb2816ea365c71de07180b14d7a1e1a92f5ff50b
David Mojica <davidmojicav@gmail.com>
2024-05-05 11:58:29 -0500

    Informe final -done

--- main/models.py
+++ main/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete, post_save, post_delete
+from django.db.models.signals import pre_delete, post_save
 from django.core.files.storage import default_storage
-from django.forms import ValidationError
 
 class TipoUsuario(models.Model):
     id = models.IntegerField(primary_key=True)
