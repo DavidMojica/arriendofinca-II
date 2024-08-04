a29c4315b4269d328c80f2b2f1537ec8d1c9d65d
David Mojica <davidmojicav@gmail.com>
2024-08-04 12:06:05 -0500

    Migrations

--- main/models.py
+++ main/models.py
@@ -55,8 +55,6 @@ class ArriendoVenta(models.Model):
     def __str__(self):
         return self.description
     
-
-
 class Pais(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=50)
